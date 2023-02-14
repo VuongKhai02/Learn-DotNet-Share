113c4047126fbd66febf62d0607eb8343b7a717b
VuongKhai02 <khaiitnoi@gmail.com>
2023-02-14 23:30:51 +0700

    New

---
+++ dotnet/btapbuoi1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace btapbuoi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.Unicode;
+
+
+            //Viết chương trình thực hiện các yêu cầu sau:
+            //Khai báo biến để lưu trữ thông tin của nhân viên bao gồm(Mã nhân viên, họ tên, ngày sinh, địa chỉ, điện thoại, email).
+            //Hiển thị thông tin của nhân viên đó trên màn hình.
+
+                //Console.WriteLine("Nhập vào mã nhân viên: ");
+                //string maNV = Console.ReadLine();
+                //Console.WriteLine("Nhập vào họ tên: ");
+                //string name = Console.ReadLine();
+                //Console.WriteLine("Nhập vào ngày sinh: ");
+                //DateTime.TryParse(Console.ReadLine(), out DateTime ngaysinh);
+                //Console.WriteLine("Nhập vào địa chỉ: ");
+                //string address = Console.ReadLine();
+                //Console.WriteLine("Nhập vào sđt: ");
+                //string sdt = Console.ReadLine();
+                //Console.WriteLine("Nhập vào email: ");
+                //string email = Console.ReadLine();
+                //Console.WriteLine("Mã nhân viên: " + maNV);
+                //Console.WriteLine("Họ tên: " + name);
+                //Console.WriteLine("Ngày sinh: " + ngaysinh);
+                //Console.WriteLine("Địa chỉ: " + address);
+                //Console.WriteLine("Email: " + email);
+
+
+            //Viết chương trình thực hiện yêu cầu sau
+            //Khai báo biến để lưu trữ thông tin kết quả học tập của sinh viên bao gồm(điểm toán, điểm văn, điểm tiếng anh).
+            //Hiển thị điểm TB, xếp loại của sinh viên trên màn hình theo quy tắc: 
+            //Điểm TB< 5: loại trung bình
+            //5 <= Điểm TB < 8: loại khá
+            //Điểm TB > 8: loại giỏi
+                Console.WriteLine("Chương trình tính điểm TB! ");
+                Console.WriteLine("Vui lòng nhập điểm phẩy với ký tự ','");
+                Console.WriteLine("Nhập điểm toán: ");
+                var diemToan = float.Parse(Console.ReadLine());
+                Console.WriteLine("Nhập điểm Anh: ");
+                var diemAnh = float.Parse(Console.ReadLine());
+                Console.WriteLine("Nhập điểm Văn: ");
+                var diemVan = float.Parse(Console.ReadLine());
+                Console.WriteLine("Điểm vừa nhập là: {0} , {1} , {2} ", diemToan,diemAnh, diemVan);
+
+                float diemTBC = (diemToan + diemAnh + diemVan) / 3 ;
+                Console.WriteLine("Điểm TBC là:  " + diemTBC);
+                Console.WriteLine("Xếp loại tương ứng là:  ");
+                if(diemTBC >= 8)
+                {
+                    Console.WriteLine("Giỏi");
+                }
+                else if (diemTBC >= 5)
+                {
+                    Console.WriteLine("Khá");
+                }
+                else
+                {
+                    Console.WriteLine("Trung bình");
+                }
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ dotnet/buoi1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Dynamic;
+using System.Text;
+
+namespace buoi1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.Unicode;
+
+            //string name = "Khải";
+            //var ten = "devmaster";
+            //const double pi = 3.14;
+            //DateTime dob = new DateTime(year: 2022, month: 10, day: 20);
+            ///* Khai bóa biến bằng var */
+            ///* Sẽ căn cứ vào biến được khai báo bên vế phải, tự gán kiểu dữ liệu */
+
+            ////Hiển thị ngày sinh theo format
+            //Console.WriteLine("Ngày sinh:" + dob.ToString(format:"dd-MM-yyyy"));
+
+            ////Hiển thị ngày sinh đầy đủ
+            //// 2 cách hiển thị ra màn hình : Nối chuỗi và sử dụng 0,1
+            //Console.WriteLine("Tên: {0}, Ngày {1}", name, dob);
+            //Console.WriteLine("name" + name+"Tuổi" + dob);
+
+            ////Lấy dữ liệu ng dùng nhập
+            //Console.WriteLine("Nhập tên của bạn: ");
+            //string name1 = Console.ReadLine();
+            //Console.WriteLine("Tên của bạn là: " + name1);
+
+            //Console.WriteLine("Nhập số của bạn muốn: ");
+            //var numStr = Console.ReadLine();
+            //var num = int.Parse(numStr);
+            //Console.Write("Số bạn vừa nhập là số: ");
+            //Console.WriteLine(num % 2 == 0 ? "chăn" : "Lẻ");
+
+            //// yy-mm-dd || dd-mm-yy 
+            //Console.WriteLine("Nhập ngày sinh:");
+            //var dateStr = Console.ReadLine();
+            //var date = DateTime.Parse(dateStr);
+            //Console.WriteLine("Mày sinh ngày: " + date);    
+            ////Tính tuổi
+            //Console.WriteLine("Tuổi của mày là: " + ((DateTime.Now).Year - date.Year));
+
+            ////Sử dụng trypare thay vì parse bình thường.
+            //Console.WriteLine("Nhập số của bạn muốn: ");
+            //var numStrr = Console.ReadLine();
+            //var status = int.TryParse(numStrr   , out int numm);
+            //Console.WriteLine("TRẠNG THÁI: " + status);
+            //Console.WriteLine("Sau khi convert: " + numm);
+
+            bool isQuit = false;
+            while (isQuit == false)
+            {
+                Console.WriteLine("Nhập số: ");
+                int.TryParse(Console.ReadLine(), out int num);
+
+                Console.Write("Sô bạn vừa nhập là: " + num + " và đó là số: ");
+                Console.WriteLine(num % 2 == 0 ? "Chẵn" : "Lẻ");
+
+                bool isValid = false;
+                while(isValid == false)
+                {
+                    Console.WriteLine("Bạn muốn nhập tiếp?  (c/k? ");
+                    string ans = Console.ReadLine();
+                    if (ans == "c")
+                    {
+                        isQuit = false;
+                        isValid = true;
+                    }
+                    else if (ans == "k")
+                    {
+                        isValid = true;
+                        isQuit = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Vui lòng nhập đúng câu trả lời (c/k)?");
+                        isValid = false;    
+                    }
+                }
+            }
+
+            //Q&A: Hỏi a Dũng
+            //Console.WriteLine("Nhập số: ");
+            //var isBool = bool.TryParse(Console.ReadLine(), out bool num);
+
+            //Console.Write("Trạng thái: " + isBool);
+            //Console.Write("Sô bạn vừa nhập là: " + num);
+
+        }
+    }
+}
